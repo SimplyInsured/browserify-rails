884427dc8bab50b457affd91264c8014b2c01bab
Charles Szymanski <charles@simplyinsured.com>
2016-12-08 17:11:39 -0800

    Work on fixing exorcist to work with honeybadger

--- lib/browserify-rails/browserify_processor.rb
+++ lib/browserify-rails/browserify_processor.rb
@@ -204,6 +204,7 @@ module BrowserifyRails
       # ignoring -o. If this happens, we give out stdout instead.
       # If we're using exorcist, then we directly use its output
       if uses_exorcist && exorcist_stdout.present?
+        exorcist_stdout = exorcist_stdout + '\n//# sourceMappingURL=application.min.js.map\n//# sourceRoot=https://sources.my-domain.com/src'
         exorcist_stdout
       elsif stdout.present?
         stdout
